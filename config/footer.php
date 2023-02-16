1ee6905c1843f2a683cc9b47f7af69d3ff66c2fa
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-02-17 03:20:42 +0530

    PG Page Has Been Completed

--- config/footer.php
+++ config/footer.php
@@ -1,3 +1,33 @@
+<footer>
+    <div id="wrapper">
+    <div class='flex-container'>
+        <div class='flex' style='flex: 2;'>
+        <br><br>
+            <img src='assets/img/logo-invert.png' width="150px">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur nulla ac dui tempor, at hendrerit nisl semper. Integer consectetur, justo vulputate efficitur ultrices, arcu lacus vestibulum nulla, sit amet consectetur nisi nibh sed augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla eleifend tincidunt ligula, eu ullamcorper mauris elementum quis. Maecenas luctus nibh in ipsum egestas, ac sodales erat posuere. Nullam commodo metus in lacinia fermentum.</p>
+        </div>
+        <div class='flex' style='flex: 1;'>
+        <h5>GENERAL</h5>
+        <ul>
+            <li><a href='profile.php'>About</a></li>
+            <li><a href='profile.php'>Terms & Conditions</a></li>
+            <li><a href='profile.php'>User Policy</a></li>
+            <li><a href='profile.php'>Privacy Policy</a></li>
+            <li><a href='profile.php'>Contact Us</a></li>
+        </ul>
+        </div>
+        <div class='flex' style='flex: 1;'>
+        <h5>SITEMAP</h5>
+        <ul>
+            <li><a href='profile.php'>Home</a></li>
+            <li><a href='profile.php'>My Account</a></li>
+            <li><a href='profile.php'>Orders</a></li>
+            <li><a href='profile.php'>Settings</a></li>
+        </ul>
+        </div>
+    </div>
+</div>
+</footer>
 <script src="https://kit.fontawesome.com/70edc5e2f3.js" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
---
+++ page.php
@@ -0,0 +1,104 @@
+<?php include_once("config/header.php"); ?>
+<link rel="stylesheet" href="./assets/css/page.css" type="text/css">
+    <br><br><br>
+    <div id="wrapper">
+            <div class="more_wrapper">
+                <h1 style='font-weight: 500; font-size: 28px; line-height: 0px;'>Whispering Pines PG</h1>
+                <a href='#' style='color: #000; font-size: 14px;'><i class="fa-solid fa-location-crosshairs"></i> Jaipur, Rajasthan, India</a>
+                <br><br>
+            <div class="carousel" data-flickity='{ "autoPlay": true, "wrapAround": true, "pageDots": false }'>
+  <div class="carousel-cell" style="width: 100%; height: 400px; background-color: #317431; background-image: url('assets/img/room (2).jpg'); background-size: cover;">
+  <div class="carousel-overlay2">
+    </div>
+  </div>
+  <div class="carousel-cell" style="width: 100%; height: 400px; background-color: #317431; background-image: url('assets/img/room (2).jpg'); background-size: cover;">
+  <div class="carousel-overlay2">
+    </div>
+  </div>
+</div>
+<div class="flex_container_page">
+    <div class="flex" style='flex: 7; background: #fff; margin: 20px 10px; border-radius: 10px;'>
+        <h3>Description</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <br><hr></hr><br>
+        <div class='inner_flex'>
+            <div class='inner_flex_area'>
+        <h3><i class="fa-solid fa-user"></i> Contact Person</h3>
+        <p>Himanshu Sharma</p>
+</div>                <div class='inner_flex_area'>
+        <h3><i class="fa-solid fa-qrcode"></i> Unique Code</h3>
+        <p>LXBGNAM10</p>
+</div>    
+<div class='inner_flex_area'>
+        <h3><i class="fa-solid fa-qrcode"></i> City</h3>
+        <p>Mumbai</p>
+</div>    
+    </div>
+    <br><hr></hr><br>
+    <h3>Address</h3>
+    <p> A - WING 1303 CELESTIAL , TANK ROAD, Lal Bahadur Shastri Marg, Bhandup West, Mumbai, Maharashtra 400078</p>    
+    <br><hr></hr><br>
+    <div style="width: 100%"><iframe class="map" style="border-radius: 12px;" width="100%" height="520" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=520&amp;hl=en&amp;q=Mumbai+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">distance maps</a></iframe></div>
+</div>
+    <div class="flex" style='padding: 0px; flex: 3; margin: 20px 10px; border-radius: 10px;'>
+        <div class='booking_area'>
+        <h3 style='line-height: 25px;'>Enquire Now!</h3>
+        <label for="date_selection">BOOKING DATE</label>
+            <input type="date" name="date_selection" class="input">
+<br><br>
+            <label for="date_selection">Your Query?</label>
+            <textarea name="query" class="query"></textarea>
+            <button type="submit">Send</button>
+        </div>
+    </div>
+    </div>
+</div>
+<h2 style="color: #222;">Find <f style='font-weight: 300;'>More</f></h2>
+        <div class="carousel" data-flickity='{ "autoPlay": true, "wrapAround": true }'>
+  <div class="carousel-cell" style="background-color: #317431; background-image: url('assets/img/room (1).jpg'); background-size: cover;">
+  <div class="carousel-overlay">
+    <div class="name-location">
+        <h3>Himanshu's PG</h3>
+        <p style='display: inline; color: #fff;'><i class="fa-solid fa-location-crosshairs"></i> Udaipur</p><span>₹3,000</span>
+    </div>
+    </div>
+  </div>
+  <div class="carousel-cell" style="background-color: #317431; background-image: url('assets/img/room (2).jpg'); background-size: cover;">
+  <div class="carousel-overlay">
+    <div class="name-location">
+        <h3>Himanshu's PG</h3>
+        <p style='display: inline; color: #fff;'><i class="fa-solid fa-location-crosshairs"></i> Udaipur</p><span>₹3,000</span>
+    </div>
+    </div>
+  </div>
+  <div class="carousel-cell" style="background-color: #317431; background-image: url('assets/img/room (3).jpg'); background-size: cover;">
+  <div class="carousel-overlay">
+    <div class="name-location">
+        <h3>Himanshu's PG</h3>
+        <p style='display: inline; color: #fff;'><i class="fa-solid fa-location-crosshairs"></i> Udaipur</p><span>₹3,000</span>
+    </div>
+    </div>
+  </div>
+  <div class="carousel-cell" style="background-color: #317431; background-image: url('assets/img/room (4).jpg'); background-size: cover;">
+  <div class="carousel-overlay">
+    <div class="name-location">
+        <h3>Himanshu's PG</h3>
+        <p style='display: inline; color: #fff;'><i class="fa-solid fa-location-crosshairs"></i> Udaipur</p><span>₹3,000</span>
+    </div>
+    </div>
+  </div>
+  <div class="carousel-cell" style="background-color: #317431; background-image: url('assets/img/room (5).jpg'); background-size: cover;">
+  <div class="carousel-overlay">
+    <div class="name-location">
+        <h3>Himanshu's PG</h3>
+        <p style='display: inline; color: #fff;'><i class="fa-solid fa-location-crosshairs"></i> Udaipur</p><span>₹3,000</span>
+    </div>
+    </div>
+  </div>
+  </div>
+            </div>
+        </div>
+</div>
+</div>
+<br><br>
+<?php include_once("config/footer.php"); ?>

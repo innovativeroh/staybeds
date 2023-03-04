f5087172c0c815e647c21c3356ea9928f95b3843
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-03-05 03:25:02 +0530

    Add Rooms System Has Been Developed

--- edit-rooms.php
+++ edit-rooms.php
@@ -52,7 +52,78 @@
                 <div style='width: 100%; background: #fff; box-sizing: border-box; border-radius: 10px; padding: 10px 30px;'>
                     <button chref="" data-modaal-content-source="#add_room" data-modaal-type="inline" data-modaal-animation="fade" class="modaal add_button"><i class="fa-solid fa-circle-plus" style='color: #4ca64c;'></i> Add Room</button>
                     <h2>Rooms Management</h2>
-                    
+                    <?php
+                        $sql = "SELECT * FROM `pages_room` WHERE `connection`='$id'";
+                        $query = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($query);
+                        if($count > "0") {
+                            while($rows = mysqli_fetch_assoc($query)) {
+                                $data_id = $rows['id'];
+                                $data_nor = $rows['number_of_room'];
+                                $data_ac_cont = $rows['ac_cont'];
+                                if($data_ac_cont == '1') {
+                                    $data_ac_cont = '<i class="fa-solid fa-snowflake"></i>';
+                                } else {
+                                    $data_ac_cont = '';
+                                }
+                                $data_electricity_water_cont = $rows['electricity_water_cont'];
+                                if($data_electricity_water_cont == '1') {
+                                    $data_electricity_water_cont = '<i class="fa-solid fa-faucet"></i> <i class="fa-solid fa-bolt"></i>';
+                                } else {
+                                    $data_electricity_water_cont = '';
+                                }
+                                $data_dth_cont = $rows['dth_cable_cont'];
+                                if($data_dth_cont == '1') {
+                                    $data_dth_cont = '<i class="fa-solid fa-tv"></i>';
+                                } else {
+                                    $data_dth_cont = '';
+                                }
+                                $data_security_deposit = $rows['security_deposit'];
+                                $data_monthly_rent = $rows['monthly_rent'];
+                                $data_sharing_type = $rows['sharing_type'];
+                                $data_amenities = $rows['amenities'];
+                                ?>
+                    <div class='room_box_area'>
+                            <div class='flex_box'>    
+                            <p style='font-size: 14px; font-weight: 500; color: #333;'><?=$data_sharing_type?></p>
+                            <?php
+                                $split = explode(',', $data_amenities);
+                                for($i=0; $i<sizeof($split); $i++){
+                                    echo "<p class='spilited_comma'>".$split[$i]."</p>";
+                                }
+                                ?>
+                                <p style='font-size: 12px; color: #888;'>Available from today</p>
+                                <p style='color: #222; font-weight: 700; font-size: 14px;'><?=$data_nor?> <f style='font-weight: 300;'> Rooms</f>
+                            </div>
+                            <div class='flex_box'>
+                                <p style='color: #222; font-weight: 700; font-size: 14px;'>₹ <?=$data_monthly_rent?> <f style='font-weight: 300;'>/ monthly rent</f>
+                                <p style='color: #222; font-weight: 700; font-size: 14px;'>₹ <?=$data_security_deposit?> <f style='font-weight: 300;'>/ security deposit</f>
+                                <p style='font-size: 14px; font-weight: 500; color: #333;' title='There are included!'><?=$data_ac_cont?> <?=$data_electricity_water_cont?> <?=$data_dth_cont?></p>
+                                <form action='edit-rooms.php?code=<?=$token?>' method='POST'>
+                                                <?php
+                        if(isset($_POST['delete'.$data_id])) {
+                            $sql = "DELETE FROM `pages_room` WHERE id='$data_id'";
+                            $query = mysqli_query($conn, $sql);
+                            echo "<meta http-equiv=\"refresh\" content=\"0; url=edit-rooms.php?code=$token\">";
+                            exit();
+                        }
+                    ?>
+                                <button type='submit' name='delete<?=$data_id?>' class='delete_box_area'>Delete</button>
+                            </form>
+                            </p>
+                            </div>
+                    </div>
+                                <?php
+                            }
+                        } else {
+                     ?>
+                     <div 
+                     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <center><lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_cgfdhxgx.json"  background="transparent"  speed="1"  style="width: 40%;" autoplay></lottie-player>
+    <br><h2 style='color: #b4bcc9;'>No rooms are avaliable!</h4><br>
+                     <?php
+                        }
+                    ?>
                 </div>
             </div>
             <div id="add_room" style="display:none;">
@@ -67,7 +138,23 @@
                             $dth_cont = @$_POST['dth_cont'];
                             $pg_month_rent = @$_POST['pg_month_rent'];
                             $pg_security_deposit = @$_POST['pg_security_deposit'];
+                            $pg_amenities = @$_POST['amenities'];
+                            if(isset($_POST['save_pg'])) {    
+                            if($ac_rent_cont == '') {
+                                $ac_rent_cont = "0";
+                            }
+                                if($electricity_water_cont == '') {
+                                $electricity_water_cont = "0";
+                            }
+                            if($dth_cont == '') {
+                                $dth_cont = "0";
+                            }
+                                $sql = "INSERT INTO `pages_room`(`id`, `number_of_room`, `ac_cont`, `electricity_water_cont`, `dth_cable_cont`, `security_deposit`, `monthly_rent`, `sharing_type`, `amenities`, `connection`) VALUES (null,'$num_of_room','$ac_rent_cont','$electricity_water_cont','$dth_cont','$pg_security_deposit','$pg_month_rent','$sharing_type','$pg_amenities','$id')";
+                            $query = mysqli_query($conn, $sql);
+                            echo "<meta http-equiv=\"refresh\" content=\"0; url=edit-rooms.php?code=$token\">";
+                            }
                         ?>
+                        <form action='edit-rooms.php?code=<?=$token?>' method='POST'>
                     <div class='current_page_cont'>
                     <h2>Add Rooms To Property</h2>
                     <p class='label_area'>Sharing Type <span style='color: red; display: inline;'>*</span></p>
@@ -104,9 +191,9 @@
                     <label for="8+">8+</label>
                 </div>
                 <hr style='margin: 20px 0px; height: 1px; border: 0 none; background: #f1f1f1;'>
-                <input type='checkbox' name='ac_rent_cont'> AC Charges Included?<br>
-                <input type='checkbox' name='electricity_water_cont'> Electricity and water charges included<br>
-                <input type='checkbox' name='dth_cont'> DTH cable charges included
+                <input type='checkbox' name='ac_rent_cont' value='1'> AC Charges Included?<br>
+                <input type='checkbox' name='electricity_water_cont' value='1'> Electricity and water charges included<br>
+                <input type='checkbox' name='dth_cont' value='1'> DTH cable charges included
                 <hr style='margin: 20px 0px; height: 1px; border: 0 none; background: #f1f1f1;'>
                 <p class='label_area'>Monthly Rent <span style='color: red; display: inline;'>*</span></p>
                 <input type='number' name='pg_month_rent' placeholder='3000' class='huge_input' required>
@@ -116,13 +203,17 @@
                 <input type='number' name='pg_security_deposit' placeholder='1500' class='huge_input' required>
 <br><br>
 <p class='label_area'>Amenities <span style='color: red; display: inline;'>*</span></p>
-                <input type='text' name='amenities' class='huge_input' onclick='change_page_fun()' placeholder='Select Amentities'>
+                <input type='text' name='amenities' id='amenities' class='huge_input' onclick='change_page_fun()' placeholder='Select Amentities'>
                 <br><br>
 <button type="submit" name="save_pg" class="button_styler">Save</button>
+                        </form>
 </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
 <div class='other_page_cont' style='display: none;'>
 <h2><button class='config_back_btn' onclick='change_page_func()'><i class="fa-solid fa-arrow-left-long"></i></button> Select Amenities</h2>
+
 <div class='amenities_flex_cont'>
     <div id='flex'>
         <p><i class="fa-solid fa-tv"></i> Television</p>
@@ -144,7 +235,7 @@
         <p><i class="fa-solid fa-snowflake"></i> Air Conditioner</p>
     </div>
     <div id='flex'>
-        <input type='checkbox' value='Fan' class='checkbox_inp'>
+        <input type='checkbox' value='Air Conditioner' class='checkbox_inp'>
     </div>
 </div>
 <div class='amenities_flex_cont'>
@@ -152,7 +243,7 @@
         <p><i class="fa-solid fa-wifi"></i> Wifi Internet</p>
     </div>
     <div id='flex'>
-        <input type='checkbox' value='Fan' class='checkbox_inp'>
+        <input type='checkbox' value='Wifi Internet' class='checkbox_inp'>
     </div>
 </div>
 <div class='amenities_flex_cont'>
@@ -160,10 +251,26 @@
         <p><i class="fa-solid fa-mattress-pillow"></i> Matress</p>
     </div>
     <div id='flex'>
-        <input type='checkbox' value='Fan' class='checkbox_inp'>
+        <input type='checkbox' value='Matress' class='checkbox_inp'>
     </div>
 </div>
 <br>
+<input type="text" name="text" id="pre_amenities" style="visibility: hidden;">
+<script>
+let $checks = $('.checkbox_inp');
+
+$checks.on('click', function() {
+
+  let values = $checks
+    .filter(':checked')
+    .map((k, box) => box.value)
+    .toArray()
+    .join(',');
+
+  $("#pre_amenities").val(values);
+
+});
+    </script>
 <button type="button" name="save_pg" class="button_styler" onclick='selection_button()'>Select</button>
 </div>
             </div>
@@ -173,6 +280,8 @@
 <script type='text/javascript'>
 let current_cont = document.querySelector(".current_page_cont");
 let other_cont = document.querySelector(".other_page_cont");
+let pre_amenities = document.querySelector("#pre_amenities");
+let amenities = document.querySelector("#amenities");
 function change_page_fun() {
     current_cont.style.display = 'none';
     other_cont.style.display = 'block';
@@ -184,5 +293,6 @@ function change_page_func() {
 function selection_button() {
     current_cont.style.display = 'block';
     other_cont.style.display = 'none';
+    amenities.value = pre_amenities.value;
 }
 </script>
